98feaabb512227a0ad43a059925a7e3b21eb69a6
Jaffarson <jaffarson2000@gmail.com>
2024-01-19 16:24:13 +0530

    youtube-clone

---
+++ src/components/VideoDetail.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { typography, Box, Stack, Typography } from "@mui/material";
+import { CheckCircle, SettingsInputSvideoSharp } from "@mui/icons-material";
+
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+  );
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
+    );
+  }, [id]);
+
+  if (!videoDetail?.snippet) return "Loading...";
+
+  console.log(videoDetail, id)
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail;
+
+  return (
+    <Box minHeight="95vh">
+      hii
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+
+            <Typography color="#fff" variant="h5" fontWeight="bold" px={2} py={1}>
+              {title}
+            </Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{
+                color: "#fff",
+              }}
+              px={2}
+            >
+              <Link to={`/channel/${channelId}`}>
+                <Typography color="#fff" variant={{ sm: "subtitle", md: "h6" }}>
+                  {channelTitle}
+                  <CheckCircle
+                    sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+                  />
+                </Typography>
+              </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography variant="body1" color="#fff" sx={{ opacity: 0.7 }}>
+                  {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+                <Typography variant="body1" color="#fff" sx={{ opacity: 0.7 }}>
+                  {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
+            </Stack>
+          </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="centter"
+          alignItems="center"
+        >
+          <Videos videos={videos} direction="column" />
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default VideoDetail;
